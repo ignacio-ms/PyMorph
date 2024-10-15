2c1141bfc9a2f0b82ac5f21ed930ecca70747c58
imarcoss <ignacio.txete@gmail.com>
2024-10-15 14:53:04 +0200

    Cellpose fix

--- nuclei_segmentation/my_cellpose.py
+++ nuclei_segmentation/my_cellpose.py
@@ -185,8 +185,8 @@ def predict(
         channels=channels,
         do_3D=kwargs['do_3D'] if 'do_3D' in kwargs else True,
         stitch_threshold=kwargs['stitch_threshold'] if 'stitch_threshold' in kwargs else .6,
-        cellprob_threshold=kwargs['cellprob_threshold'] if 'cellprob_threshold' in kwargs else .4,
-        flow_threshold=kwargs['flow_threshold'] if 'flow_threshold' in kwargs else .2,
+        cellprob_threshold=kwargs['cellprob_threshold'] if 'cellprob_threshold' in kwargs else .1,
+        flow_threshold=kwargs['flow_threshold'] if 'flow_threshold' in kwargs else .3,
         verbose=verbose
     )
 
--- scripts/cluster2tierra.sh
+++ scripts/cluster2tierra.sh
@@ -2,5 +2,5 @@
 
 for i in {1..11}
 do
-    rsync -avhP --progress --inplace --no-perms imarcoss@10.149.80.48:/data_lab_MT/Ignacio/data/data//Gr${i}/Segmentation/Nuclei/*.nii.gz /run/user/1003/gvfs/smb-share:server=tierra.cnic.es,share=sc/LAB_MT/LAB/Ignacio/Gr${i}/Segmentation/Nuclei/
+    rsync -vhP --progress --no-perms --ignore-times imarcoss@10.149.80.48:/data_lab_MT/Ignacio/data/data//Gr${i}/Segmentation/Nuclei/*.nii.gz /run/user/1003/gvfs/smb-share:server=tierra.cnic.es,share=sc/LAB_MT/LAB/Ignacio/Gr${i}/Segmentation/Nuclei/
 done
