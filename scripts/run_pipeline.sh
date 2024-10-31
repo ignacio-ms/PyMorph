8e44fd825deb2d79711d6df0de0b38ea1051f859
imarcoss <ignacio.txete@gmail.com>
2024-10-31 17:38:53 +0100

    Cell ids face mapping 0.5

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -17,14 +17,22 @@ conda activate py310ml
 #python feature_extraction/run_extractor.py -l 'myocardium' -t 'Membrane' -v 1
 #python feature_extraction/run_extractor.py -l 'splanchnic' -t 'Membrane' -v 1
 
-python meshes/run_mesh.py -t 'myocardium' -l 'Nuclei' -v 1 -a 1
-python meshes/run_mesh.py -t 'splanchnic' -l 'Nuclei' -v 1 -a 1
+#python meshes/run_mesh.py -t 'myocardium' -l 'Nuclei' -v 1 -a 1
+#python meshes/run_mesh.py -t 'splanchnic' -l 'Nuclei' -v 1 -a 1
+#
+#python meshes/run_mesh.py -t 'myocardium' -l 'Membrane' -v 1 -a 1
+#python meshes/run_mesh.py -t 'splanchnic' -l 'Membrane' -v 1 -a 1
 
-python meshes/run_mesh.py -t 'myocardium' -l 'Membrane' -v 1 -a 1
-python meshes/run_mesh.py -t 'splanchnic' -l 'Membrane' -v 1 -a 1
+python filtering/run_filter_tissue.py -t 'myocardium' -l 'Membrane' -v 1
+python filtering/run_filter_tissue.py -t 'splanchnic' -l 'Membrane' -v 1
 
-#python meshes/run_extractor_complex.py -l 'Membrane' -t 'myocardium' -v 1 -m 1
-#python meshes/run_extractor_complex.py -l 'Membrane' -t 'splanchnic' -v 1 -m 1
+# NOT RUN
+#python filtering/run_filter_tissue.py -t 'myocardium' -l 'Nuclei' -v 1
+#python filtering/run_filter_tissue.py -t 'splanchnic' -l 'Nuclei' -v 1
 
+python meshes/run_extractor_complex.py -l 'Membrane' -t 'myocardium' -v 1 -m 1
+python meshes/run_extractor_complex.py -l 'Membrane' -t 'splanchnic' -v 1 -m 1
+
+# NOT RUN
 #python meshes/run_extractor_complex.py -l 'Nuclei' -t 'myocardium' -v 1 -m 1
 #python meshes/run_extractor_complex.py -l 'Nuclei' -t 'splanchnic' -v 1 -m 1
