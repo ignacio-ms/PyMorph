46a08af9a8dc582da8f2a978bad7a7265b938ba5
imarcoss <ignacio.txete@gmail.com>
2024-11-14 18:44:52 +0100

    tissue2atlas map coloring 0.5

---
+++ scripts/tierra2cluster.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..11}
+do
+    rsync -avhP --progress --no-perms --ignore-times /run/user/1003/gvfs/smb-share:server=tierra.cnic.es,share=sc/LAB_MT/LAB/Ignacio/Gr${i}/ imarcoss@10.149.80.48:/data_lab_MT/Ignacio/data/data//Gr${i}/
+done
