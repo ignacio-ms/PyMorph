5396df82f42be5f8c508a44170fcc9fa88626f88
imarcoss <ignacio.txete@gmail.com>
2024-11-21 15:52:06 +0100

    Local PC surface map

--- scripts/run_nuclei_correction.sh
+++ scripts/run_nuclei_correction.sh
@@ -3,9 +3,11 @@
 source /home/imarcoss/mambaforge/etc/profile.d/conda.sh
 conda activate py310ml
 
+#"0806_E5" "0504_E1" "0208_E2" "0521_E4" "0521_E2"
+#"0516_E2" "0503_E1" "0208_E3"
+
 specimens=(
-    "0806_E5" "0504_E1" "0208_E2" "0521_E4" "0521_E2"
-    "0516_E2" "0503_E1" "0209_E1" "0520_E2" "0208_E3"
+    "0209_E1" "0520_E2"
     "0209_E2" "0308_E3" "0503_E2" "0516_E3"
     "0521_E1" "0523_E1" "0806_E3" "0806_E4" "0806_E6"
     "0401_E3" "0123_E1" "0122_E1" "0518_E3" "0521_E3"
@@ -17,23 +19,23 @@ specimens=(
     "0515_E3"
 )
 
-for specimen in "${specimens[@]}"
-do
-  python feature_extraction/run_extractor.py -s $specimen -l 'myocardium' -t 'Nuclei' -v 1
-  python feature_extraction/run_extractor.py -s $specimen -l 'splanchnic' -t 'Nuclei' -v 1
-
-  python feature_extraction/run_extractor.py -s $specimen -l 'myocardium' -t 'Nuclei' -v 1 -n 1
-  python feature_extraction/run_extractor.py -s $specimen -l 'splanchnic' -t 'Nuclei' -v 1 -n 1
-
-  python meshes/run_mesh.py -s $specimen -t 'myocardium' -l 'Nuclei' -v 1
-  python meshes/run_mesh.py -s $specimen -t 'splanchnic' -l 'Nuclei' -v 1
-
-#  python filtering/run_filter_tissue.py -s $specimen -t 'myocardium' -l 'Nuclei' -v 1
-#  python filtering/run_filter_tissue.py -s $specimen -t 'splanchnic' -l 'Nuclei' -v 1
-
-#  python meshes/run_extractor_complex.py -s $specimen -l 'Nuclei' -t 'myocardium' -v 1 -m 1
-#  python meshes/run_extractor_complex.py -s $specimen -l 'Nuclei' -t 'splanchnic' -v 1 -m 1
-done
+#for specimen in "${specimens[@]}"
+#do
+#  python feature_extraction/run_extractor.py -s $specimen -l 'myocardium' -t 'Nuclei' -v 1
+#  python feature_extraction/run_extractor.py -s $specimen -l 'splanchnic' -t 'Nuclei' -v 1
+#
+#  python feature_extraction/run_extractor.py -s $specimen -l 'myocardium' -t 'Nuclei' -v 1 -n 1
+#  python feature_extraction/run_extractor.py -s $specimen -l 'splanchnic' -t 'Nuclei' -v 1 -n 1
+#
+#  python meshes/run_mesh.py -s $specimen -t 'myocardium' -l 'Nuclei' -v 1
+#  python meshes/run_mesh.py -s $specimen -t 'splanchnic' -l 'Nuclei' -v 1
+#
+##  python filtering/run_filter_tissue.py -s $specimen -t 'myocardium' -l 'Nuclei' -v 1
+##  python filtering/run_filter_tissue.py -s $specimen -t 'splanchnic' -l 'Nuclei' -v 1
+#
+##  python meshes/run_extractor_complex.py -s $specimen -l 'Nuclei' -t 'myocardium' -v 1 -m 1
+##  python meshes/run_extractor_complex.py -s $specimen -l 'Nuclei' -t 'splanchnic' -v 1 -m 1
+#done
 
 for specimen in "${specimens[@]}"
 do
